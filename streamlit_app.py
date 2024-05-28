1ed7380a9a547e693c9490e4545aa94449cd3a68
SreekuttyGH <sreekutty.ks@alzonesoftware.com>
2024-05-28 12:07:53 +0530

    Update streamlit_app.py
    api-8

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,9 @@ from snowflake.snowpark.functions import col
 cnx=st.connection("snowflake")
 #session = get_active_session()
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect('choose upto 5 ingredients:',my_dataframe)
